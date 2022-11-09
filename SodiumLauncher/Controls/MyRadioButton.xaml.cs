88e84f849d3001fd93991d51a91c4dd8eba82a88
HRxiaohu <HRxiaohu1@163.com>
2022-11-09 16:00:12 +0800

    调整UI

--- SodiumLauncher/Controls/MyRadioButton.xaml.cs
+++ SodiumLauncher/Controls/MyRadioButton.xaml.cs
@@ -13,21 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SodiumLauncher
+namespace SodiumLauncher.Controls
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// UserControl1.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class UserControl1 : UserControl
     {
-        public MainWindow()
+        public UserControl1()
         {
             InitializeComponent();
-        }
-
-        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            this.DragMove();
         }
     }
 }
