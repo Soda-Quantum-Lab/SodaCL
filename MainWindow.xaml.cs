92ce3d2fc19e320224c0e126195e060da2d77560
Int256 <73740872+tripleInt@users.noreply.github.com>
2022-11-13 01:39:49 +0800

    feat: save and load info from launcher's files.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 using Sodium_Launcher.Main.Minecraft;
 
 namespace Sodium_Launcher
@@ -23,6 +25,10 @@ namespace Sodium_Launcher
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<MCClient> clients = new List<MCClient>();
+        private string _versionListSavePath = @".\SDL\versions.json";
+        private string _launcherInfoSavePath = @".\SDL\launcher.json";
+        LauncherInfo launcherInf;
         public MainWindow()
         {
             InitializeComponent();
@@ -30,6 +36,19 @@ namespace Sodium_Launcher
         #region 自定义标题栏
         private void Exit_Button_Click(object sender, RoutedEventArgs e)
         {
+            if(!File.Exists(_versionListSavePath))
+            {
+                FileStream fileStream = new FileStream(_versionListSavePath, FileMode.Create, FileAccess.ReadWrite);
+                fileStream.Close();
+            }
+            File.WriteAllText(_versionListSavePath, JsonConvert.SerializeObject(clients));
+
+            if(!File.Exists(_launcherInfoSavePath))
+            {
+                FileStream fileStream = new FileStream(_launcherInfoSavePath, FileMode.Create, FileAccess.ReadWrite);
+                fileStream.Close();
+            }
+            File.WriteAllText(_launcherInfoSavePath, JsonConvert.SerializeObject(launcherInf));
             this.Close();
         }
 
@@ -45,11 +64,37 @@ namespace Sodium_Launcher
             string _MCDir = @".\.minecraft";
             try
             {
-                if (!Directory.Exists(_SDLDir) || !Directory.Exists(_MCDir))
+                if (!Directory.Exists(_SDLDir))
                 {
                     Directory.CreateDirectory(_SDLDir);
+                }
+
+                if (!!Directory.Exists(_MCDir))
+                {
                     Directory.CreateDirectory(_MCDir);
                 }
+
+                if (!File.Exists(_versionListSavePath))
+                {
+                    FileStream fileStream = new FileStream(_versionListSavePath, FileMode.Create, FileAccess.ReadWrite);
+                    fileStream.Close();
+                }
+                else
+                {
+                    clients = JsonConvert.DeserializeObject<List<MCClient>>(File.ReadAllText(_versionListSavePath));
+                }
+
+                if (!File.Exists(_launcherInfoSavePath))
+                {
+                    FileStream fileStream = new FileStream(_launcherInfoSavePath, FileMode.Create, FileAccess.ReadWrite);
+                    fileStream.Close();
+                    this.launcherInf = new LauncherInfo();
+                }
+                else
+                {
+                    this.launcherInf = JsonConvert.DeserializeObject<LauncherInfo>(File.ReadAllText(_launcherInfoSavePath));
+                }
+                this.launcherInf.addLaunchTime(); // 启动器启动次数统计
             }
             catch(Exception ex)
             {
