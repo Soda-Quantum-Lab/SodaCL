5731dd98abf015b8bb3a3f343c33c1d4cdc5279c
HRxiaohu <HRxiaohu1@163.com>
2022-11-28 17:40:05 +0800

    feat: 添加启动窗口

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,12 @@ namespace SodaCL
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            SplashScreen splashScreen = new SplashScreen("/Resources/Images/Dev.ico");
+            splashScreen.Show(true);
+            base.OnStartup(e);
+
+        }
     }
 }
