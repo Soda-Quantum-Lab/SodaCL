06491154c8ec407e1c975e27bb5d04b3cc4ee338
HRxiaohu <63327671+HRxiaohu@users.noreply.github.com>
2022-12-21 12:52:58 +0800

    feat:调整绑定

--- SodaCL/Launcher/LauncherInit.cs
+++ SodaCL/Launcher/LauncherInit.cs
@@ -1,47 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using static SodaCL.Launcher.LauncherLogging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SodaCL.Launcher
 {
     class LauncherInit
     {
-        LauncherInfo launcherInfo;
-        private void InitNewFolder()
-        {
-            try
-            {
-                if (!File.Exists(LauncherInfo.versionListSavePath))
-                {
-                    FileStream fileStream = new(LauncherInfo.versionListSavePath, FileMode.Create, FileAccess.ReadWrite);
-                    fileStream.Close();
-                    Log(ModuleList.IO, LogInfo.Info, "新建版本文件");
-                }
-                else
-                {
-                    clients = JsonConvert.DeserializeObject<List<MCClient>>(File.ReadAllText(LauncherInfo.versionListSavePath));
-                }
-
-                if (!File.Exists(LauncherInfo.launcherInfoSavePath))
-                {
-                    FileStream fileStream = new(LauncherInfo.launcherInfoSavePath, FileMode.Create, FileAccess.ReadWrite);
-                    fileStream.Close();
-                    Log(ModuleList.IO, LogInfo.Info, "新建启动器文件");
-                    this.launcherInfo = new LauncherInfo();
-                }
-                else
-                {
-                    this.launcherInfo = JsonConvert.DeserializeObject<LauncherInfo>(File.ReadAllText(LauncherInfo.launcherInfoSavePath));
-                }
-                this.launcherInfo.addLaunchTime(); // 启动器启动次数统计
-            }
-
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                Log(ModuleList.IO, LogInfo.Error, ex.Message, ex.StackTrace);
-            }
-        }
-        }
+    }
 }
