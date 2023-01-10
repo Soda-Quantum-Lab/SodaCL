967e978807ff473dcb6adfb2e3fed752585d1543
CNCloud_Casey <60414767+bwtx2022@users.noreply.github.com>
2023-01-10 17:23:35 +0800

    chores(核心文件夹识别): 更改实现方式，现在仅输出内含文件夹名称，而不是完整路径

--- Core/MCVersionList.cs
+++ Core/MCVersionList.cs
@@ -5,22 +5,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static SodaCL.Toolkits.Logger;
+using System.Security.Cryptography.X509Certificates;
+using SodaCL.Launcher;
 
 namespace SodaCL.Core
 {
     internal class MCVersionList
     {
-        public static void GetVersionList() 
+        public static string[] GetVersionList() 
         {
-            DirectoryInfo mcVersionsFolder = new DirectoryInfo(SodaCL.Launcher.LauncherInfo.mcVersionsDir);
-            DirectoryInfo[] mcVersionsArray = mcVersionsFolder.GetDirectories();
-            int[] mcVersionsArrayLength = new int[] { 1, 2, 3 };
-            foreach (var item in mcVersionsArray)
+            // 留着，以后有用
+            //DirectoryInfo mcVersionsFolder = new DirectoryInfo(SodaCL.Launcher.LauncherInfo.mcVersionsDir);
+            //DirectoryInfo[] mcVersionsArray = mcVersionsFolder.GetDirectories();
+            //int[] mcVersionsArrayLength = new int[] { 1, 2, 3 };
+            //foreach (var item in mcVersionsArray)
+            //{
+            //    string versionFolderPath = item.ToString;
+            //    string versionFolderName = Path.GetFileName(versionFolderPath);
+            //    Log(ModuleList.IO, LogInfo.Info, item.ToString());
+            //    Console.WriteLine(item.ToString());
+            //}
+            if (Directory.Exists(LauncherInfo.mcVersionsDir))
             {
-                Log(ModuleList.IO, LogInfo.Info, item.ToString());
-                Console.WriteLine(item.ToString());
+                string[] dir = Directory.GetDirectories(LauncherInfo.mcVersionsDir);
+                string[] names = new string[dir.Length];
+                Log(ModuleList.IO, LogInfo.Info, "查找到 versions 文件夹内核心文件夹: ");
+                for (int i = 0; i < dir.Length; i++)
+                {
+                    names[i] = Path.GetFileName(dir[i]);
+                    Log(ModuleList.IO, LogInfo.Info, names[i]);
+                }
+                return names;
             }
-
+            else
+            {
+                Log(ModuleList.IO, LogInfo.Error, "versions 目录不存在, 可能是初始化阶段出现了异常导致 versions 文件夹未成功创建");
+                return null;
+            }
         }
         
         
