5637d87244d78873d4a4b2430b5acc3e54179486
CNCloud_Casey <60414767+bwtx2022@users.noreply.github.com>
2023-01-14 18:00:32 +0800

    chore: 添加 Launch 文件（啥都没有）

---
+++ Core/Game/MC_Launch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaCL.Core.Game
+{
+    internal class MC_Launch
+    {
+        public static void MCLaunching()
+        {
+
+        }
+    }
+}
