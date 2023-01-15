65358532e2625317978e85943ca63ed982bb7da6
CNCloud_Casey <60414767+bwtx2022@users.noreply.github.com>
2023-01-15 19:29:26 +0800

    chore: 修复一个可能导致计算机上没有 Java 时启动游戏直接崩溃的问题

--- Core/Java/JavaSelector.cs
+++ Core/Java/JavaSelector.cs
@@ -14,31 +14,84 @@ namespace SodaCL.Core.Java
         public static Array SelectJavaByVersion(int gameVersion)
         {
             string[] javaPathArray = (string[])AutoJavaFinding(true);
-            if (gameVersion < 1.17)
+            if (javaPathArray == null)
             {
-                IEnumerable<string> javaIsCompatiable = from java in javaPathArray
-                                            where java.Contains("8")
-                                            select java;
-                string[] javaIsCompatiableArray = javaIsCompatiable.ToArray();
-                Log(false, ModuleList.IO, LogInfo.Info, "SodaCL 找到了以下符合核心版本要求的 Java : ");
-                for (int i = 0; i < javaIsCompatiableArray.Length; i++)
-                {
-                    Log(false, ModuleList.IO, LogInfo.Info, javaIsCompatiableArray[i]);
-                }
-                return javaIsCompatiableArray;
+                Log(true, ModuleList.IO, LogInfo.Info, "SodaCL 找不到 Java 。\n请检查你的计算机上是否安装了 Java 。");
+                return null;
             }
             else
             {
-                IEnumerable<string> javaIsCompatiable = from java in javaPathArray
-                                                        where java.Contains("17")
-                                                        select java;
-                string[] javaIsCompatiableArray = (string[])javaIsCompatiable;
-                Log(false, ModuleList.IO, LogInfo.Info, "SodaCL 找到了以下符合核心版本要求的 Java : ");
-                for (int i = 0; i < javaIsCompatiableArray.Length; i++)
+                if (gameVersion < 1.17)
                 {
-                    Log(false, ModuleList.IO, LogInfo.Info, javaIsCompatiableArray[i]);
+                    if (gameVersion == 1.16)
+                    {
+                        IEnumerable<string> javaIsCompatiable11 = from java in javaPathArray
+                                                                where java.Contains("11")
+                                                                select java;
+                        if (javaIsCompatiable11 == null)
+                        {
+                            IEnumerable<string> javaIsCompatiable8 = from java in javaPathArray
+                                                                    where java.Contains("8")
+                                                                    select java;
+                            if (javaIsCompatiable8 == null)
+                            {
+                                Log(true, ModuleList.IO, LogInfo.Info, "SodaCL 找不到符合条件的 Java 。");
+                                return null;
+                            }
+                            else
+                            {
+                                string[] javaIsCompatiableArray = javaIsCompatiable8.ToArray();
+                                Log(false, ModuleList.IO, LogInfo.Info, "SodaCL 找到了以下符合核心版本要求的 Java : ");
+                                for (int i = 0; i < javaIsCompatiableArray.Length; i++)
+                                {
+                                    Log(false, ModuleList.IO, LogInfo.Info, javaIsCompatiableArray[i]);
+                                }
+                                return javaIsCompatiableArray;
+                            }
+                        }
+                        else
+                        {
+                            string[] javaIsCompatiableArray = javaIsCompatiable11.ToArray();
+                            Log(false, ModuleList.IO, LogInfo.Info, "SodaCL 找到了以下符合核心版本要求的 Java : ");
+                            for (int i = 0; i < javaIsCompatiableArray.Length; i++)
+                            {
+                                Log(false, ModuleList.IO, LogInfo.Info, javaIsCompatiableArray[i]);
+                            }
+                            return javaIsCompatiableArray;
+                        }
+                    }
+                    else
+                    {
+                        IEnumerable<string> javaIsCompatiable = from java in javaPathArray
+                                                                where java.Contains("8")
+                                                                select java;
+                        string[] javaIsCompatiableArray = javaIsCompatiable.ToArray();
+                        Log(false, ModuleList.IO, LogInfo.Info, "SodaCL 找到了以下符合核心版本要求的 Java : ");
+                        for (int i = 0; i < javaIsCompatiableArray.Length; i++)
+                        {
+                            Log(false, ModuleList.IO, LogInfo.Info, javaIsCompatiableArray[i]);
+                        }
+                        return javaIsCompatiableArray;
+                    }
                 }
-                return javaIsCompatiableArray;
+                else
+                {
+                    IEnumerable<string> javaIsCompatiable17 = from java in javaPathArray
+                                                            where java.Contains("17")
+                                                            select java;
+                    if (javaIsCompatiable17 == null)
+                    {
+                        Log(true, ModuleList.IO, LogInfo.Info, "SodaCL 找不到符合条件的 Java 。");
+                        return null;
+                    }
+                    string[] javaIsCompatiableArray = (string[])javaIsCompatiable17;
+                    Log(false, ModuleList.IO, LogInfo.Info, "SodaCL 找到了以下符合核心版本要求的 Java : ");
+                    for (int i = 0; i < javaIsCompatiableArray.Length; i++)
+                    {
+                        Log(false, ModuleList.IO, LogInfo.Info, javaIsCompatiableArray[i]);
+                    }
+                    return javaIsCompatiableArray;
+                }
             }
         }
     }
