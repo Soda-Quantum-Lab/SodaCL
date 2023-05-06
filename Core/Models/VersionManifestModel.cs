abe2b190a38e6dff164031f3588ff51762fe02ba
HRxiaohu <HRxiaohu1@outlook.com>
2023-05-06 17:00:13 +0800

    feat：Some small fixes

--- Core/Models/VersionManifestModel.cs
+++ Core/Models/VersionManifestModel.cs
@@ -8,6 +8,9 @@ using Newtonsoft.Json;
 
 namespace SodaCL.Core.Models
 {
+	/// <summary>
+	/// Minecraft 版本类型
+	/// </summary>
 	public enum VersionType
 	{
 		Snapshot,
@@ -15,6 +18,9 @@ namespace SodaCL.Core.Models
 	}
 	public class VersionManifestModel
 	{
+		/// <summary>
+		/// 最近的 Minecraft 版本号
+		/// </summary>
 		[JsonProperty("latest")]
 		public LatestModel Latest { get; set; }
 		[JsonProperty("versions")]
@@ -22,17 +28,53 @@ namespace SodaCL.Core.Models
 	}
 	public class LatestModel
 	{
+		/// <summary>
+		/// 最近的 Minecraft 正式版版本号
+		/// </summary>
 		[JsonProperty("release")]
 		public string Release { get; set; }
+		/// <summary>
+		/// 最近的 Minecraft 快照版版本号
+		/// </summary>
 		[JsonProperty("snapshot")]
 		public string Snapshot { get; set; }
 	}
 	public class VersionModel
 	{
+		/// <summary>
+		/// Minecraft 版本 Id
+		/// </summary>
 		[JsonProperty("id")]
 		public string Id { get; set; }
+		/// <summary>
+		/// Minecraft 版本类型
+		/// </summary>
 		[JsonProperty("type")]
 		public VersionType Type { get; set; }
-
+		/// <summary>
+		/// Minecraft 配置文件地址
+		/// </summary>
+		[JsonProperty("url")]
+		public string Url { get; set; }
+		/// <summary>
+		/// Minecraft 发布时间 (ISO 8601-1:2019 格式)
+		/// </summary>
+		[JsonProperty("time")]
+		public string Time { get; set; }
+		/// <summary>
+		/// Minecraft 构建时间 (ISO 8601-1:2019 格式)
+		/// </summary>
+		[JsonProperty("releaseTime")]
+		public string ReleaseTime { get; set; }
+		/// <summary>
+		/// Minecraft Sha1 字符串
+		/// </summary>
+		[JsonProperty("sha1")]
+		public string Sha1 { get; set; }
+		/// <summary>
+		/// TODO:弄清楚这是啥
+		/// </summary>
+		[JsonProperty("complianceLevel")]
+		public short ComplianceLevel { get; set; }
 	}
 }
